a23542e44e9bc7f95d5968ee9c08da85f9d7d537
Brian Galiwango <byanreynolds70@gmail.com>
2022-10-27 10:15:19 +0300

    update

--- init_db.py
+++ init_db.py
@@ -2,7 +2,7 @@ import psycopg2
 import os
 
 #Init database   
-DATABASE_URL = os.environ["POSTGRES_DATABASE_URL"]
+DATABASE_URL = os.environ["DATABASE_URL"]
      
 conn = psycopg2.connect(DATABASE_URL)
 
